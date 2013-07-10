da33729dfd926ed3c2d648f751ef26e1c44d5c3e
mhornak <mhornak@gmail.com>
2013-07-10 09:15:08 +0000

    Update to web.js, trying to get fs and buffer working

--- web.js
+++ web.js
@@ -4,11 +4,10 @@ var app = express.createServer(express.logger());
 
 fs.readFileSync("index.html", "utf-8", function(err, data) {
     if(err) throw err;
+    app.get('/', function(request, response){
+	response.send(data.toString());
+    })
 })
-
-app.get('/', function(request, response) {
-  response.send(data.toString());
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
