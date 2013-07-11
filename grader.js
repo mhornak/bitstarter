fb220ce70458f39a85160357f67cc661a1017b55
mhornak <mhornak@gmail.com>
2013-07-11 18:25:20 +0000

    Updated to accept URL

--- grader.js
+++ grader.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -23,7 +24,7 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
+var checkHtmlFile = function( htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
@@ -42,8 +43,17 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json' , clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u --url <URL>', 'URL to check')
 	.parse(process.argv);
 
+    if(program.url){
+	rest.get(program.url).on('complete', function(result) {
+	    fs.writeFile(__dirname + '/graderurl.html', result, function(err) {
+		if (err) throw err;
+	    });
+	    program.file = 'graderurl.html';
+	});
+    }
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
